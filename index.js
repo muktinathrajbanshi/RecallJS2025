0aaa7866ac6344cad222ef286adb75b61514a842
unknown <muktinathrajbanshi786@gmail.com>
2025-12-07 11:44:28 +0530

    Values & Variable, Naming, Data types covered

--- index.js
+++ index.js
@@ -1,4 +1,56 @@
-console.log("Hello World!");
+//?1 Print Hello World
+// console.log("Hello World!");
 
-var myname = "Muktinath Rajbanshi";
-console.log(myname);
+//?2 Values & Variable
+// var myName = "Muktinath Rajbanshi";
+// var age = 30;
+
+//?3 Naming Practice
+// var _myName = "muktinath";
+// var 1myName = "muktinath rajbanshi";
+// var _1my__Name = "rajbanshi";
+// var $myName = "welcome";
+// var myName% = "muktinath";
+// console.log(myName%);
+
+//?4 Data Types in JavaScript
+// var myName = "Muktinath Rajbanshi";
+// console.log(myName);
+
+// var myAge = 23;
+// console.log(myAge);
+
+// var iAmMuktis = false;
+// console.log(iAmMuktis);
+
+// typeof operator 
+// console.log(typeof(iAmMuktis));
+
+//? DataTypes Practice 
+// console.log( 10 + "20");
+// console.log(9 - "5"); //bug
+
+// console.log("Java " + "Script");
+
+// console.log(" " + " ");
+
+// console.log(" " + 0);
+
+// console.log("muktinath" - "rajbanshi");
+
+// console.log(true + true);
+
+// console.log(true + false);
+
+// console.log(false + true);
+
+console.log(false - true);
+
+
+
+
+
+
+
+
+
