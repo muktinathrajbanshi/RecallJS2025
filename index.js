0de7ee858a6681b3a32d3f2c35070362046a8619
unknown <muktinathrajbanshi786@gmail.com>
2025-12-08 14:34:20 +0530

    wow finally all challenges are solved

--- index.js
+++ index.js
@@ -214,25 +214,36 @@
 //? 3. Write a program to swap two numbers.
 //? 4. Write a program to swap two numbers without using third variable.
 
-// sol 1: 
+// sol 1: ✅
 // console.log(3**3); //3*3*3
 
-// sol 2:
+// sol 2: ✅
 // console.log(5 - "muktinath");
 
-// sol 3: 
+// sol 3: ✅
+// var a = 5;
+// var b = 10;
+
+// output b=5; a=10
+// var c = b; // c = 10
+// b = a; // b = 5;
+// a = c;
+// console.log("the value of a is " + a);
+// console.log("the value of b is " + b);
+
+// sol 4: ✅
+
 var a = 5;
 var b = 10;
 
 // output b=5; a=10
-var c = b; // c = 10
-b = a; // b = 5;
-a = c;
+
+a = a+b; // a = 15
+b = a - b; // b = 5
+a = a - b; // a =10
+
 console.log("the value of a is " + a);
 console.log("the value of b is " + b);
-
-
-
 
 
 
