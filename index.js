58d66f2544925ec02b552fd08c99214549a6dd4e
unknown <muktinathrajbanshi786@gmail.com>
2025-12-09 13:42:55 +0530

    wow finally switch statement is completed

--- index.js
+++ index.js
@@ -337,23 +337,42 @@
 //? 1. Without break statement
 // Find the Area of circle, triangle and rectangle?
 
-var area = "square";
+// var area = "square";
+// var PI = 3.142, l=5, b=4, r=3;
+
+// if(area == "circle") {
+//     console.log("the area of circle is : " + PI*r**2);  
+// } else if(area == "triangle") {
+//     console.log("the area of triangle is : " + (l*b)/2);
+// } else if(area == "rectangle") {
+//     console.log("the area of rectangle is : " + (l*b));
+// } else {
+//     console.log("please enter valid data");
+// }
+
+var area = "ggg";
 var PI = 3.142, l=5, b=4, r=3;
 
-if(area == "circle") {
-    console.log("the area of circle is : " + PI*r**2);  
-} else if(area == "triangle") {
-    console.log("the area of triangle is : " + (l*b)/2);
-} else if(area == "rectangle") {
-    console.log("the area of rectangle is : " + (l*b));
-} else {
-    console.log("please enter valid data");
+
+switch(area) {
+    case "circle":
+       console.log("the area of circle is : " + PI*r**2); 
+       break;
+    case "triangle":
+        console.log("the area of triangle is : " + (l*b)/2);
+         break;
+    case "rectangle":
+         console.log("the area of rectangle is : " + (l*b));
+          break;
+    default:
+       console.log("please enter valid data");  
 }
 
 
-
-
-
+// 🤫break
+// Terminates the current loop, switch, or label
+// statement and transfers
+// program control to the statement following the terminated statement.
 
 
 
