d52625fceda66713c1fb74ab7019722f2c36669c
unknown <muktinathrajbanshi786@gmail.com>
2025-12-12 08:28:36 +0530

    wow 2 challenges are completed

--- index.js
+++ index.js
@@ -760,7 +760,18 @@
     //? 2. What is the return value of splice method?
     //? 3. update march to March (updata)?
     //? 4. Delete June from an array?
+
+    const months = ["Jan", "march", "April", "June", "July"];
+
+    //? soln1:
+    const newMonth = months.splice(months.length, 0, "Dec");
+    // console.log(months);
+
+    //? soln2:
+    console.log(newMonth);
     
+    
+
     
     
 
