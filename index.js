d2fb7bbcc7ffabcf041734a844bf3ff475cf3eeb
unknown <muktinathrajbanshi786@gmail.com>
2025-12-13 06:36:39 +0530

    now let's focus on Reduce() method

--- index.js
+++ index.js
@@ -840,13 +840,24 @@
     // console.log(arrSqr);
 
     //? soln2:
-    let arr = [2, 3, 4, 6, 8];
+    // let arr = [2, 3, 4, 6, 8];
 
-    let arr2 = arr.map((curElem) => curElem * 2).filter((curElem) => curElem > 10);
-    console.log(arr2);
+    // let arr2 = arr.map((curElem) => curElem * 2).filter((curElem) => curElem > 10);
+    // console.log(arr2);
     
+     //! 8.4 👉 Reduce Method
+    //? flatten an array means to convert the 3d or 2d array into a single dimensional array
 
-   
+    //? The reduce() method executes a reducer function (that you provide) on each element of the
+    //? array, resulting in single output value.
+
+    //? The reducer function takes four arguments:
+
+    //? Accumulator
+    //? Current Value
+    //? Current Index
+    //? Source Array
+    
     
 
     
