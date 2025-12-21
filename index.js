d5e707f0fdf2e44f21fab8f402660dc1089d190a
unknown <muktinathrajbanshi786@gmail.com>
2025-12-21 10:12:35 +0530

    wow 4 ways of writing Events in JS is noted down

--- index.js
+++ index.js
@@ -1405,7 +1405,12 @@
 
     //? HTML allows event handler attributes, with JavaScript code, to be added to HTML elements.
 
-    
+    //?! 👉 4 ways of writing Events in JavaScript
+
+    //? 1. using alert();
+    //? 2: By Calling a function (We already seen and most common way of writing)
+    //? 3: using Inline events (HTML onclick="" property and element.onclick)
+    //? 4: using Event Listeners (addEventListener and IE's attachEvent)
     
     
     
