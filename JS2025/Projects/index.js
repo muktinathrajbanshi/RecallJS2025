7af5135f2beb16a2896c80606677d689cb03738d
unknown <muktinathrajbanshi786@gmail.com>
2025-12-25 22:02:59 +0545

    wow 4 time interval are noted down man

--- JS2025/Projects/index.js
+++ JS2025/Projects/index.js
@@ -1448,6 +1448,13 @@
     //? setTimeout(function, milliseconds)
     //? Executes a function, after waiting a specified number of milliseconds.
     
+    //? setInterval(function, milliseconds)
+    //? Same as setTimeout(), but repeats the execution of the function continuously.
+
+    //? 1. setTimeout()
+    //? 2. clearTimeout()
+    //? 3. setInterval()
+    //? 4. clearInterval()
     
   
   
