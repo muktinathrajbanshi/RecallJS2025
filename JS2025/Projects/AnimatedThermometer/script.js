94db2957428d1feec2ce9111128655c76c41e9db
unknown <muktinathrajbanshi786@gmail.com>
2026-01-02 17:28:16 +0545

    wow that awesome man

--- JS2025/Projects/AnimatedThermometer/script.js
+++ JS2025/Projects/AnimatedThermometer/script.js
@@ -7,6 +7,7 @@ const tempLoad = () => {
 
     setTimeout(() => {
         temp.innerHTML = "&#xf2ca";
+        temp.style.color = "#f8b627";
     }, 1000);
 
      setTimeout(() => {
@@ -16,6 +17,11 @@ const tempLoad = () => {
      setTimeout(() => {
         temp.innerHTML = "&#xf2c8";
     }, 3000);
+
+     setTimeout(() => {
+        temp.innerHTML = "&#xf2c7";
+        temp.style.color = "#d63031";
+    }, 4000);
 
 }
 
