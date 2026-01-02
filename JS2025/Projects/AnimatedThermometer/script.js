3116ea0eb4ba04197141d8d173efdaf83a03caef
unknown <muktinathrajbanshi786@gmail.com>
2026-01-02 17:20:38 +0545

    wow that awesome man

--- JS2025/Projects/AnimatedThermometer/script.js
+++ JS2025/Projects/AnimatedThermometer/script.js
@@ -3,6 +3,20 @@
 const tempLoad = () => {
     let temp = document.getElementById("temp");
     temp.innerHTML = "&#xf2cb";
+
+
+    setTimeout(() => {
+        temp.innerHTML = "&#xf2ca";
+    }, 1000);
+
+     setTimeout(() => {
+        temp.innerHTML = "&#xf2c9";
+    }, 2000);
+
+     setTimeout(() => {
+        temp.innerHTML = "&#xf2c8";
+    }, 3000);
+
 }
 
 tempLoad();
