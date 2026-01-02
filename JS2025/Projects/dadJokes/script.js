ad5abe44425825c043cf6084edcb8684ba329e89
unknown <muktinathrajbanshi786@gmail.com>
2026-01-02 07:20:21 +0545

    wow through Async-Await that mini project is completed

--- JS2025/Projects/dadJokes/script.js
+++ JS2025/Projects/dadJokes/script.js
@@ -2,7 +2,26 @@
 const jokes = document.querySelector("#joke");
 const jokeBtn = document.querySelector("#jokeBtn");
 
-const generateJokes = () => {
+// const generateJokes = () => {
+
+//     const setHeader = {
+//         headers: {
+//             Accept : "application/json"
+//         }
+//     }
+
+//     fetch("https://icanhazdadjoke.com/", setHeader)
+//     .then((res) =>  res.json() )
+//     .then( (data) => { 
+//         jokes.innerHTML = data.joke;
+//     })
+//     .catch((error) => {
+//         console.log(error); 
+//     })
+// }
+
+
+const generateJokes = async () => {
 
     const setHeader = {
         headers: {
@@ -10,14 +29,14 @@ const generateJokes = () => {
         }
     }
 
-    fetch("https://icanhazdadjoke.com/", setHeader)
-    .then((res) =>  res.json() )
-    .then( (data) => {
+    try {
+        const res = await fetch("https://icanhazdadjoke.com/", setHeader);
+        const data = await res.json();
         jokes.innerHTML = data.joke;
-    })
-    .catch((error) => {
-        console.log(error); 
-    })
+    } catch (error) {
+        console.log(`the error is ${error}`);
+        
+    }
 }
 
 
