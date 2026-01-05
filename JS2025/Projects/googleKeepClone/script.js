145dcbdd14159452c56b3097990da7c6c1b6a3c8
unknown <muktinathrajbanshi786@gmail.com>
2026-01-05 09:58:25 +0545

    delete operation is done in this project

--- JS2025/Projects/googleKeepClone/script.js
+++ JS2025/Projects/googleKeepClone/script.js
@@ -16,7 +16,20 @@ const addNewNote = (text = '') => {
         <textarea class=""></textarea> `;
 
         note.insertAdjacentHTML("afterbegin", htmlData);
-        console.log(note);
+        // console.log(note);
+
+        // getting the References 
+        const editButton = note.querySelector(".edit");
+        const delButton = note.querySelector(".delete");
+        const mainDiv = note.querySelector(".main");
+        const textarea = note.querySelector("textarea");
+
+
+        // deleting the node 
+        delButton.addEventListener("click", () => {
+            note.remove();
+        })
+
 
         notesContainer.appendChild(note);
         
